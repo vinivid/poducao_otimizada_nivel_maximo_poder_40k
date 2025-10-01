28f0c2ea6da7e5d0e37857b685c4452373ecc15e
vinivid <frato.vini@gmail.com>
2025-10-01 14:08:17 -0300

    Resultados do gprof

--- exe3/test.sh
+++ exe3/test.sh
@@ -5,13 +5,46 @@ function rodar_sair() {
 
     ./clean
     ./koala $num_sort < vec_entrada.in
-    gprof ./koala gmon.out > $2/saida$num_run.txt
-    rm ./gmon.out
+    mv ./gmon.out $folder/gmon$num_run.out
 }
 
-for i in {1..10} 
-do 
-    rodar_sair 0 ./quicksort $i
-    rodar_sair 1 ./merge $i
-    rodar_sair 2 ./heap $i
-done 
+function sumarize() {
+    path=$1
+    name=$2
+    files=("$@")
+}
+
+function main() {
+    qui_path="./quicksort"
+    mer_path="./merge"
+    hep_path="./heap"
+
+    for i in {1..10} 
+    do 
+        rodar_sair 0 $qui_path $i
+        rodar_sair 1 ./merge $i
+        rodar_sair 2 ./heap $i
+    done
+
+    qui_files=($qui_path/*.out)
+    mer_files=($mer_path/*.out)
+    hep_files=(./heap/*.out)
+
+    gprof ./koala -s ${qui_files[@]}
+    gprof ./koala gmon.sum > $qui_path/quick_resultados.txt
+    rm gmon.sum
+
+    gprof ./koala -s ${mer_files[@]}
+    gprof ./koala gmon.sum > $mer_path/mer_resultados.txt
+    rm gmon.sum
+
+    gprof ./koala -s ${hep_files[@]}
+    gprof ./koala gmon.sum > $hep_path/heap_resultados.txt
+    rm gmon.sum
+
+    printf "%s\n" "${qui_files[@]}" | xargs rm 
+    printf "%s\n" "${mer_files[@]}" | xargs rm 
+    printf "%s\n" "${hep_files[@]}" | xargs rm 
+}
+
+main
