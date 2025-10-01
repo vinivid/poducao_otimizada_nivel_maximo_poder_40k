60481d5f454f99724344f69e69ad583b363ab974
vinivid <frato.vini@gmail.com>
2025-10-01 10:52:12 -0300

    Testar com gmon criado

--- exe3/test.sh
+++ exe3/test.sh
@@ -1,6 +1,17 @@
+function rodar_sair() {
+    num_sort=$1
+    folder=$2
+    num_run=$3
+
+    ./clean
+    ./koala $num_sort < vec_entrada.in
+    gprof ./koala gmon.out > $2/saida$num_run.txt
+    rm ./gmon.out
+}
+
 for i in {1..10} 
 do 
-    ./clean
-    ./koala
-    
+    rodar_sair 0 ./quicksort $i
+    rodar_sair 1 ./merge $i
+    rodar_sair 2 ./heap $i
 done 
